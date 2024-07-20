8e7168134754443f7e35aefdfb70dca0ea24a6ed
hyejinggu <118355536+hyejinggu@users.noreply.github.com>
2024-07-20 12:41:16 +0900

    클래스의 분리와 인터페이스

---
+++ src/main/java/hyejin/tobyspring/ExRateProvider.java
@@ -0,0 +1,8 @@
+package hyejin.tobyspring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public interface ExRateProvider {
+    BigDecimal getExRate(String currency) throws IOException;
+}
--- src/main/java/hyejin/tobyspring/PaymentService.java
+++ src/main/java/hyejin/tobyspring/PaymentService.java
@@ -1,28 +1,24 @@
 package hyejin.tobyspring;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-abstract public class PaymentService {
+public class PaymentService {
+        private final ExRateProvider exRateProvider;
+
+        public PaymentService() {
+            this.exRateProvider = new SimpleExRateProvider();
+        }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
+
+        BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
-
-    abstract BigDecimal getExRate(String currency) throws IOException;
 
 
 
